44cca20b71297903f34e0e15b7bb4c16522f5e20
mohamed-tantawy <mohamed.eltantawy@cairo-tech.net>
2022-01-14 17:30:04 +0200

    added try catch to products handler

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -5,13 +5,23 @@ import { Product, ProductStore } from "../models/product";
 const store = new ProductStore();
 
 const index = async (_req: Request, res: Response) => {
-  const products = await store.index();
-  res.json(products);
+  try {
+    const products = await store.index();
+    res.json(products);
+  } catch (error) {
+    res.status(400);
+    res.json(error as string);
+  }
 };
 
 const show = async (_req: Request, res: Response) => {
-  const product = await store.show(_req.params.id);
-  res.json(product);
+  try {
+    const product = await store.show(_req.params.id);
+    res.json(product);
+  } catch (error) {
+    res.status(400);
+    res.json(error as string);
+  }
 };
 
 const create = async (req: Request, res: Response) => {
